13a28b897ddd1b2261c36810b4deeea118ff7522
TanTrisGit <tyson@sent.com>
2023-12-29 14:59:28 +0100

    New strategy: game class ('KP') and every move produces new object of that class

--- KhunPhan2.py
+++ KhunPhan2.py
@@ -1,15 +1,13 @@
 import numpy as np
 import unittest as ut
 
-# This program aims to solve Khun Phan, a riddle where different pieces move around a field.
-# Two different representations of the field are used here (for now).
-    # One is a 4-by-5 list of lists (5 lists with each having 4 entries) with bool values for whether the field is occupied.
-    # The other is an enumeration of all fields from 1 to 20, starting with fields 1-4 in the first row.
+# This program aims to solve Khun Phan, a riddle where different pieces move around a board.
+# Two different representations of the board are used here (for now).
+    # One is a 4-by-5 list of lists (5 lists with each having 4 entries) with bool values for whether the board is occupied.
+    # The other is an enumeration of all boards from 1 to 20, starting with fields 1-4 in the first row.
     # The pieces are listed in four lists, one for each type, values from 1 to 20 in each list tell the position of the pieces' upper left corners
     # List gets updated on each move, and then sorted to be comparable to past positions
     # Formula to transform matrix index to enumerated position: posin20(i,j) = (i-1) * 4 + j
-# TODO: It might be possible to represent everything just with the numbers from the second version, 
-# so one long-term goal is to switch the whole computation to that.
 
 # A piece on the Khun Phan board has a type and i/j-coordinates (row/column)
 class Piece : 
@@ -26,37 +24,42 @@ class Piece :
     
     # Return a list of (i,j)-tuples of the squares that are being occupied by this piece.
     def occupies(self) :
-        if self.t == 1 : 
-            return [(self.i, self.j)]
-        elif self.t == 2 :
-            return [(self.i, self.j), (self.i + 1, self.j)]
-        elif self.t == 3 :
-            return [(self.i, self.j), (self.i, self.j + 1)]
-        else :
-            return [(self.i, self.j), (self.i, self.j + 1), 
+        match self.t :
+            case 1 :
+                return [(self.i, self.j)]
+            case 2 :
+                return [(self.i, self.j), (self.i + 1, self.j)]
+            case 3 :
+                return [(self.i, self.j), (self.i, self.j + 1)]
+            case 4 :
+                return [(self.i, self.j), (self.i, self.j + 1), 
                     (self.i + 1, self.j), (self.i + 1, self.j + 1)]
         
     # Return list of bool values whether it is possible for a piece to move upwards, to the right, down or to the left
     def getMoves(self, f) :
-        if self.t == 1 : 
-            return [f.getCell(self.i - 1, self.j), f.getCell(self.i, self.j + 1), f.getCell(self.i + 1, self.j), f.getCell(self.i, self.j - 1)]
-        elif self.t == 2 :
-            return [f.getCell(self.i - 1, self.j), f.getCell(self.i, self.j + 1) and f.getCell(self.i + 1, self.j + 1), 
-                    f.getCell(self.i + 2, self.j), f.getCell(self.i, self.j - 1) and f.getCell(self.i + 1, self.j - 1)]
-        elif self.t == 3 :
-            return [f.getCell(self.i - 1, self.j) and f.getCell(self.i - 1, self.j + 1), f.getCell(self.i, self.j + 1), 
-                    f.getCell(self.i + 1, self.j) and f.getCell(self.i + 1, self.j + 1), f.getCell(self.i, self.j - 1)]
-        else :
-            return [f.getCell(self.i - 1, self.j) and f.getCell(self.i - 1, self.j + 1), 
-                    f.getCell(self.i, self.j + 2) and f.getCell(self.i + 1, self.j + 2),                     
-                    f.getCell(self.i + 2, self.j) and f.getCell(self.i + 2, self.j + 1), 
-                    f.getCell(self.i, self.j - 1) and f.getCell(self.i + 1, self.j - 1)]
+        match self.t :
+            case 1 : 
+                return [f.getCell(self.i - 1, self.j), f.getCell(self.i, self.j + 1), 
+                        f.getCell(self.i + 1, self.j), f.getCell(self.i, self.j - 1)]
+            case 2 :
+                return [f.getCell(self.i - 1, self.j), f.getCell(self.i, self.j + 1) and f.getCell(self.i + 1, self.j + 1), 
+                        f.getCell(self.i + 2, self.j), f.getCell(self.i, self.j - 1) and f.getCell(self.i + 1, self.j - 1)]
+            case 3 :
+                print([f.getCell(self.i - 1, self.j) and f.getCell(self.i - 1, self.j + 1), f.getCell(self.i, self.j + 1), 
+                        f.getCell(self.i + 1, self.j) and f.getCell(self.i + 1, self.j + 1), f.getCell(self.i, self.j - 1)])
+                return [f.getCell(self.i - 1, self.j) and f.getCell(self.i - 1, self.j + 1), f.getCell(self.i, self.j + 1), 
+                        f.getCell(self.i + 1, self.j) and f.getCell(self.i + 1, self.j + 1), f.getCell(self.i, self.j - 1)]
+            case 4 :
+                return [f.getCell(self.i - 1, self.j) and f.getCell(self.i - 1, self.j + 1), 
+                        f.getCell(self.i, self.j + 2) and f.getCell(self.i + 1, self.j + 2),                     
+                        f.getCell(self.i + 2, self.j) and f.getCell(self.i + 2, self.j + 1), 
+                        f.getCell(self.i, self.j - 1) and f.getCell(self.i + 1, self.j - 1)]
 
 
     def getType(self) :
         return self.t
 
-    # Move this piece on a given field in a direction: up(1), right(2), down(3), left(4)
+    # Move this piece on a given board in a direction: up(1), right(2), down(3), left(4)
     # TODO: change if-cases to match/case statement (everywhere in this program actually)
     def move(self, dir) :
         if dir == 1 :
@@ -69,16 +72,16 @@ class Piece :
             self.j -= 1
 
 
-# The Khun Phan playing field (maybe rename to 'board'?)
-class Field :
+# The Khun Phan playing board 
+class Board :
     height = 5
     width = 4
 
-    # Initialising the field with a matrix of bool values (True = empty, False = occupied) and a list of the pieces on the field.
-    # In order to avoid having to deal with index-out-of-bounds problems, the field is surrounded
+    # Initialising the board with a matrix of bool values (True = empty, False = occupied) and a list of the pieces on the board.
+    # In order to avoid having to deal with index-out-of-bounds problems, the board is surrounded
     # by a padding of 'False' values, implying that no piece can be put there.
     def __init__(self) :
-        self.field = [[False, False, False, False, False, False],
+        self.board = [[False, False, False, False, False, False],
                       [False, True, True, True, True, False],
                       [False, True, True, True, True, False],
                       [False, True, True, True, True, False],
@@ -87,7 +90,7 @@ class Field :
                       [False, False, False, False, False, False]]
 
     def __repr__(self) -> str :
-        return str(np.array(self.field))
+        return str(np.array(self.board))
     
     def setPieceList(self, list) :
         self.pieceList = list
@@ -96,10 +99,10 @@ class Field :
         return self.pieceList
 
     def setCell(self, i, j, val) :
-        self.field[i][j] = val
+        self.board[i][j] = val
 
     def getCell(self, i, j) :
-        return self.field[i][j]
+        return self.board[i][j]
 
     def updatePieceList(self, t, n, d) :
         # How to change the number in the piecelist according to move direction
@@ -110,20 +113,12 @@ class Field :
         self.pieceList[t-1].sort()
 
 
-# ------------------------------------------------------------------------------------------
-# Now that we have pieces and a field, this is the controller section working with both.
-
-# Starting position for the original Khun Phan challenge. 
-start = [[14,15,18,19], [1,4,13,16], [10], [2]]
-# Keep track of all positions that have been reached so far.
-positions = []
-
 # Node class to create a search tree. 
-# Each node consists of a position and a list of following position, its children.
-class Node :
-    def __init__(self, position, field, pieces) :
+# Each node consists of a position and a list of following positions, its children.
+class KP :
+    def __init__(self, position, board, pieces) :
         self.position = position
-        self.field = field
+        self.board = board
         self.pieces = pieces
         self.children = []
 
@@ -132,46 +127,65 @@ class Node :
 
     def getPos(self) :
         return self.position
+    
+    def getBoard(self) :
+        return self.board
+    
+    def getPieces(self) :
+        return self.pieces
 
     def getChildren(self) :
         return self.children
 
 
+# ------------------------------------------------------------------------------------------
+# Now that we have pieces, a playing board and search trees, this is the controller section.
+
+
+# Starting position for the original Khun Phan challenge. 
+start = [[14,15,18,19], [1,4,13,16], [10], [2]]
+# Keep track of all positions that have been reached so far.
+positions = [[]]
+
+# Create first objects, start search tree.
 def controller() :
-    f,ps = setup(start)
-    createTree(f,ps,start)
+    khunPhan = setup(start)
+    createTree(khunPhan)
     
-def createTree(f, ps, pos) :  
-    print(f.getPieceList())
-    positions.append(pos)
-    if len(positions) > 1024 : 
-        raise AssertionError("Reached more than 1024 different positions!")
-    node = Node(pos, f, ps)
-    for piece in ps :
-        moves = piece.getMoves(f)
+# Set up a board with pieces
+def setup(pos) :
+    board = Board()
+    pieces = []
+    board.setPieceList(pos)
+    for k in range(4):
+        for x in pos[k] :
+            piece = Piece(k+1, (x-1)//4 + 1, (x-1)%4 + 1)
+            addPiece(board, piece)
+            pieces.append(piece)
+    khunPhan = KP(pos, board, pieces)
+    return (khunPhan)
+
+def createTree(kp) :  
+    positions.append(kp.getPos())
+    if len(positions) > 900 : 
+        print(positions)
+        raise AssertionError("Reached more than 900 different positions!")
+    for piece in kp.getPieces() :
+        moves = piece.getMoves(kp.getBoard())
         for k, m in enumerate(moves) :
             if m : 
-                newPos = move(f, piece, k+1)
+                newKP = newBoard(kp, piece, k+1)
+                newPos = newKP.getPos()
                 if newPos in positions :
                     print("Position already found.")
                 elif newPos[3] == [14] : 
                     print("Solved!\n", newPos)
+                    break
                 else :
-                    print("Creating new node.")
-                    node.addChild(createTree(setup(newPos), newPos))
+                    newBoard, newPieces = setup(newPos)
+                    kp.addChild(createTree(newBoard, newPieces, newPos))
 
-# Set up a field with pieces
-def setup(pos) :
-    field = Field()
-    pieces = []
-    for k in range(4):
-        for x in pos[k] :
-            piece = Piece(k+1, (x-1)//4 + 1, (x-1)%4 + 1)
-            addPiece(field, piece)
-            pieces.append(piece)
-    field.setPieceList(pos)
-    return (field, pieces)
-
+# Add a piece to a board if there is space for it
 def addPiece(f, p) :
     occupied = p.occupies()
     for i,j in occupied : 
@@ -180,93 +194,98 @@ def addPiece(f, p) :
     for i,j in occupied:
         f.setCell(i,j,False)
 
-def move(f, p, dir) :
-    p.move(dir)
-    match dir, p.getType() :
+# Given a game, a piece and a direction, create a new board where the piece has been moved
+def newBoard(kp, p, dir) :
+    newKP = KP(kp.getPos(), kp.getBoard(), kp.getPieces())
+    for newPiece in newKP.getPieces :
+        if comparePieces(newPiece, p) :
+            newPiece.move(dir)
+    match dir, newPiece.getType() :
         case 1,1 :
-            f.setCell(p.getI()+1, p.getJ(), True)
-            f.setCell(p.getI(), p.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI()+1, newPiece.getJ(), True)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ(), False)
         case 1,2 :
-            f.setCell(p.getI()+2, p.getJ(), True)
-            f.setCell(p.getI(), p.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI()+2, newPiece.getJ(), True)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ(), False)
         case 1,3 :
-            f.setCell(p.getI()+1, p.getJ(), True)
-            f.setCell(p.getI()+1, p.getJ()+1, True)
-            f.setCell(p.getI(), p.getJ(), False)
-            f.setCell(p.getI(), p.getJ()+1, False)
+            newKP.getBoard().setCell(newPiece.getI()+1, newPiece.getJ(), True)
+            newKP.getBoard().setCell(newPiece.getI()+1, newPiece.getJ()+1, True)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ()+1, False)
         case 1,4 :
-            f.setCell(p.getI()+2, p.getJ(), True)
-            f.setCell(p.getI()+2, p.getJ()+1, True)
-            f.setCell(p.getI(), p.getJ(), False)
-            f.setCell(p.getI(), p.getJ()+1, False)
+            newKP.getBoard().setCell(newPiece.getI()+2, newPiece.getJ(), True)
+            newKP.getBoard().setCell(newPiece.getI()+2, newPiece.getJ()+1, True)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ()+1, False)
         case 2,1 :
-            f.setCell(p.getI(), p.getJ()-1, True)
-            f.setCell(p.getI(), p.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ()-1, True)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ(), False)
         case 2,2 :
-            f.setCell(p.getI(), p.getJ()-1, True)
-            f.setCell(p.getI()+1, p.getJ()-1, True)
-            f.setCell(p.getI(), p.getJ(), False)
-            f.setCell(p.getI()+1, p.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ()-1, True)
+            newKP.getBoard().setCell(newPiece.getI()+1, newPiece.getJ()-1, True)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI()+1, newPiece.getJ(), False)
         case 2,3 :
-            f.setCell(p.getI(), p.getJ()-1, True)
-            f.setCell(p.getI(), p.getJ()+1, False)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ()-1, True)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ()+1, False)
         case 2,4 :
-            f.setCell(p.getI(), p.getJ()-1, True)
-            f.setCell(p.getI()+1, p.getJ()-1, True)
-            f.setCell(p.getI(), p.getJ()+1, False)
-            f.setCell(p.getI()+1, p.getJ()+1, False)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ()-1, True)
+            newKP.getBoard().setCell(newPiece.getI()+1, newPiece.getJ()-1, True)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ()+1, False)
+            newKP.getBoard().setCell(newPiece.getI()+1, newPiece.getJ()+1, False)
         case 3,1 :
-            f.setCell(p.getI()-1, p.getJ(), True)
-            f.setCell(p.getI(), p.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI()-1, newPiece.getJ(), True)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ(), False)
         case 3,2 :
-            f.setCell(p.getI()-1, p.getJ(), True)
-            f.setCell(p.getI()+1, p.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI()-1, newPiece.getJ(), True)
+            newKP.getBoard().setCell(newPiece.getI()+1, newPiece.getJ(), False)
         case 3,3 :
-            f.setCell(p.getI()-1, p.getJ(), True)
-            f.setCell(p.getI()-1, p.getJ()+1, True)
-            f.setCell(p.getI(), p.getJ(), False)
-            f.setCell(p.getI(), p.getJ()+1, False)
+            newKP.getBoard().setCell(newPiece.getI()-1, newPiece.getJ(), True)
+            newKP.getBoard().setCell(newPiece.getI()-1, newPiece.getJ()+1, True)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ()+1, False)
         case 3,4 :
-            f.setCell(p.getI()-1, p.getJ(), True)
-            f.setCell(p.getI()-1, p.getJ()+1, True)
-            f.setCell(p.getI()+1, p.getJ(), False)
-            f.setCell(p.getI()+1, p.getJ()+1, False)
+            newKP.getBoard().setCell(newPiece.getI()-1, newPiece.getJ(), True)
+            newKP.getBoard().setCell(newPiece.getI()-1, newPiece.getJ()+1, True)
+            newKP.getBoard().setCell(newPiece.getI()+1, newPiece.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI()+1, newPiece.getJ()+1, False)
         case 4,1 :
-            f.setCell(p.getI(), p.getJ()+1, True)
-            f.setCell(p.getI(), p.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ()+1, True)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ(), False)
         case 4,2 :
-            f.setCell(p.getI(), p.getJ()+1, True)
-            f.setCell(p.getI()+1, p.getJ()+1, True)
-            f.setCell(p.getI(), p.getJ(), False)
-            f.setCell(p.getI()+1, p.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ()+1, True)
+            newKP.getBoard().setCell(newPiece.getI()+1, newPiece.getJ()+1, True)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI()+1, newPiece.getJ(), False)
         case 4,3 :
-            f.setCell(p.getI(), p.getJ()+2, True)
-            f.setCell(p.getI(), p.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ()+2, True)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ(), False)
         case 4,4 :
-            f.setCell(p.getI(), p.getJ()+2, True)
-            f.setCell(p.getI()+1, p.getJ()+2, True)
-            f.setCell(p.getI(), p.getJ(), False)
-            f.setCell(p.getI()+1, p.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ()+2, True)
+            newKP.getBoard().setCell(newPiece.getI()+1, newPiece.getJ()+2, True)
+            newKP.getBoard().setCell(newPiece.getI(), newPiece.getJ(), False)
+            newKP.getBoard().setCell(newPiece.getI()+1, newPiece.getJ(), False)
+    
+    newKP.getBoard().updatePieceList(newPiece.getType(), (newPiece.getI() - 1) * 4 + newPiece.getJ(), dir)
+    return newKP
 
-    print(p.getType(), (p.getI() - 1) * 4 + p.getJ())
-    f.updatePieceList(p.getType(), (p.getI() - 1) * 4 + p.getJ(), dir)
-    return f.pieceList
-
+def comparePieces(p1, p2) :
+    return p1.getType() == p2.getType() and p1.getI() == p2.getI() and p1.getJ() == p2.getJ()
 
 # ------------------------------------------------------------------------------------
 
 controller()
 # setup(start)
 
-# field = Field()
-# print(field)
+# board = Board()
+# print(board)
 # piece1 = Piece(1,4,2)
 # piece2 = Piece(2,1,1)
-# addPiece(field, piece1)
-# addPiece(field, piece2)
+# addPiece(board, piece1)
+# addPiece(board, piece2)
 
 
-# f1 = Field()
+# f1 = Board()
 # p1 = Piece(1, 1, 1)
 # p2 = Piece(1, 3, 4)
 # f1.addPiece(p1)
