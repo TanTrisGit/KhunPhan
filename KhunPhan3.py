dae0c180742a6044bdbac208fe10a08cd2f835a4
TanTrisGit <tyson@sent.com>
2023-12-30 14:54:16 +0100

    Did I mention it's working?

--- KhunPhan3.py
+++ KhunPhan3.py
@@ -67,7 +67,7 @@ def lookAround(state, type, n) :
         case 0 :
             return [b[i-1][j], b[i][j+1], b[i+1][j], b[i][j-1]]
         case 1 : 
-            return [b[i-1][j], b[i][j+1] and b[i+1][j+1], b[i+1][j], b[i][j-1] and b[i+1][j-1]]
+            return [b[i-1][j], b[i][j+1] and b[i+1][j+1], b[i+2][j], b[i][j-1] and b[i+1][j-1]]
         case 2 :
             return [b[i-1][j] and b[i-1][j+1], b[i][j+2], b[i+1][j] and b[i+1][j+1], b[i][j-1]]
         case 3 :
@@ -99,7 +99,7 @@ class State :
         return self.position
     
     def getHowDid(self) :
-        return self.howDid
+        return copy.deepcopy(self.howDid)
     
     def getBoard(self) :
         return self.board
@@ -129,26 +129,85 @@ class State :
 reached = []
 
 def run() :
+    # Save solutions in text document
+    solutions = open("Solutions.txt", "w")
+    solutions.write("These are my solutions for Khun Phan.\n")
+    solutions.close()
     node(State(startPosition, []))
+    
+# Calculate the distance between two pieces
+def dist(n1,n2) :
+    i1,j1 = getIJ(n1)
+    i2,j2 = getIJ(n2)
+    return (abs(i1-i2) + abs(j1-j2))
+
+def shouldVisit(pos, howDid) :
+    s0 = pos[0][0]
+    s1 = pos[0][1]
+    s2 = pos[0][2]
+    s3 = pos[0][3]
+
+    v0 = pos[1][0]
+    v1 = pos[1][1]
+    v2 = pos[1][2]
+    v3 = pos[1][3]
+
+    h = pos[2][0]
+
+    b = pos[3][0]
+
+    # If the four single squares are in one row, no real movement is possible
+    # That is the case if their (sorted) values have a difference of 3 from last to first
+    if s3 - s0 == 3 :
+        return False
+    
+    # Also, at least two of the four single squares should always be next to each other
+    # elif dist(s0,s1) > 1 and dist(s0,s2) > 1 and dist(s0,s3) > 1 and dist (s1,s2) > 1 and dist(s1,s3) > 1 and dist(s2,s3) > 1 :
+    #     return False
+      
+    # Make sure the big piece actually moves further away from its original position over time
+    elif len(howDid) > 400 :
+        return False
+    else :
+        return True
+
+def printToFile(l) :
+    solutions = open("Solutions.txt", "a")
+    solutions.write("[")
+    for part in l :
+        solutions.write(str(part))
+        solutions.write(",")
+        solutions.write("\n")
+    solutions.write("]\n")
+    solutions.close()
+
 
 def node(state) :
     curPos = state.getPosition()
-    if curPos[3] != [2] :
-        print(curPos)
+    #TESTING. TODO: Remove
+    #Testing whether the big square has moved two steps down the board
+    # if curPos[3][0] > 12 :
+    #     print(curPos)
     # Save in all positions reached
     reached.append(curPos)
-    # Save in all positions that led to this state
+    # Add to positions that led to this state
     state.addToHowDid(curPos)
     # Check winning condition
-    if curPos[3] == [14] : 
+    if curPos[3][0] == 14 : 
         print("Found a solution!")
-        print(state.getHowDid())
+        printToFile(state.getHowDid())
         return
     for s in state.successorPositions() :
-        if s not in reached :
+        if s not in reached and shouldVisit(s, state.getHowDid()):
             node(State(s, state.getHowDid()))
 
 run()
-            
-# n = State([[14, 15, 18, 19], [1, 4, 13, 16], [11], [2]], [])
-# print(n.successorPositions())
+
+ 
+
+# All of this seems to be working fine.
+# However, the maximum recursion depth is exceeded. 
+# Maybe this can be fixed with a better solution for setting up the board than deepcopying the empty board.
+# However, I will try to make the system intelligent by not visiting nodes that are probably bad.
+# That is what the shouldVisit() method is for.
+
